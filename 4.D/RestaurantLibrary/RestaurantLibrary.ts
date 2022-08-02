cdda4fa34162105d87ac54d35a4425501d284379
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-02 15:09:53 +0200

    not made yet

--- 4.D/RestaurantLibrary/RestaurantLibrary.ts
+++ 4.D/RestaurantLibrary/RestaurantLibrary.ts
@@ -1,8 +1,3 @@
-import {Oven} from "./Devices/Oven";
-import {Stove} from "./Devices/Stove";
-
-
-
 export class RestaurantLibrary{
     // opvullen met Restau's 
     private _restaurantList:Array<Restaurant>;
