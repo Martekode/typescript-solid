e3da631d62b0dc4912de4231a4ef13e5736d8693
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-02 14:34:04 +0200

    broken restaurantlib

---
+++ 4.D/RestaurantLibrary/RestaurantLibrary.ts
@@ -0,0 +1,11 @@
+import {Oven} from "./Devices/Oven";
+import {Stove} from "./Devices/Stove";
+
+
+
+export class RestaurantLibrary{
+    // opvullen met Restau's 
+    private _restaurantList:Array<Restaurant>;
+    // different Restaurants
+}
+
