75754288071bce4987c91024c0843721fdca62d6
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-02 14:33:05 +0200

    init

---
+++ 4.D/newer.ts
@@ -0,0 +1,6 @@
+import {Restaurant} from "./Restaurant";
+
+let JoeMamasBakeries  = new Restaurant();
+JoeMamasBakeries.restaurantLibrary.bakery.oven.prepare("cookies");
+JoeMamasBakeries.restaurantLibrary.bakery.crepery.prepare("crepes");
+
