22f41b1c2ff6a7b4d0283398ad993e1587eeb854
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-02 02:59:45 +0200

    created defaultDevice class and functionality

--- 4.D/new.ts
+++ 4.D/new.ts
@@ -16,7 +16,7 @@ class Oven extends CookingDevice implements ICanCook{
         super();
     }
 
-    toggleOnDevice(): void {
+    public toggleOnDevice(): void {
         setTimeout(function (){
             document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : DEVICE IS TOGGLED ON</p>";
         }, 1000);
@@ -24,7 +24,7 @@ class Oven extends CookingDevice implements ICanCook{
         this._isOn = true;
     }
 
-    toggleOffDevice(): void {
+    public toggleOffDevice(): void {
         setTimeout(function (){
             document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : DEVICE TOGGLED OFF</p><hr>";
         }, 3000);
@@ -56,11 +56,15 @@ class Stove extends CookingDevice implements ICanCook{
         super();
     }
 
-    toggleOnDevice(): void {
-        
+    public toggleOnDevice(): void {
+        setTimeout(function (){
+            document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : DEVICE IS TOGGLED ON</p>";
+        }, 1000);
+        console.log("DEVICE IS TOGGLED ON"); //insert fart humor here
+        this._isOn = true;
     }
 
-    toggleOffDevice(): void {
+    public toggleOffDevice(): void {
         setTimeout(function (){
             document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : DEVICE TOGGLED OFF</p><hr>";
         }, 3000);
@@ -85,23 +89,68 @@ class Stove extends CookingDevice implements ICanCook{
         }
     }
 }
+class DefaultDevice extends CookingDevice implements ICanCook{
+
+    constructor(){
+        super();
+    }
+
+    public toggleOnDevice(): void {
+        setTimeout(function (){
+            document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : DEVICE IS TOGGLED ON</p>";
+        }, 1000);
+        console.log("DEVICE IS TOGGLED ON"); //insert fart humor here
+        this._isOn = true;
+    }
+
+    public toggleOffDevice(): void {
+        setTimeout(function (){
+            document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : DEVICE TOGGLED OFF</p><hr>";
+        }, 3000);
+        console.log("DEVICE TOGGLED OFF"); //insert fart humor here
+        this._isOn = false;
+    }
+
+    public bake(item : string)
+    {
+        if(this._isOn) {
+            setTimeout(function (){
+                document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : Now Preparing " + item + " !</p>";
+            }, 2000);
+            console.log("Now preparing " + item + "!");
+        }
+        else
+        {
+            setTimeout(function (){
+                document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : DEVICE IS NOT TOOGLED ON</p>";
+            }, 2000);
+            console.log("DEVICE IS NOT TOGGLED ON");//insert fart humor here
+        }
+    }
+}
 class Restaurant {
     private _name : string;
-    private _cookingDevice : Object;
+    private _cookingDevice : ICanCook;
 
     constructor(name : string) {
         this._cookingDevice = this.checkDevice(name);
     }
-    private checkDevice(name : string) : Object {
+    private checkDevice(name : string) : ICanCook  {
         if (name === "Bakery"){
             return new Oven();
         }else if (name === "Crepery"){
             return new Stove(); 
-        }else{
-            return new Error("This is not a valid Restaurant");
         }
+        return new DefaultDevice();
     }
-    public get cookingDevice() : Object {
-        return this._cookingDevice;
+
+    public cook(item : string) {
+        this._cookingDevice.toggleOnDevice();
+        this._cookingDevice.bake(item);
+        this._cookingDevice.toggleOffDevice();
     }
 }
+let newBakery = new Restaurant("Bakery");
+newBakery.cook("cookies");
+let crepery = new Restaurant("Crepery");
+crepery.cook("crepes");
