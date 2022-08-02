378be111053399eda520ccf4c1291ba36fc343a1
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-02 15:09:26 +0200

    prepare food override to specific food items

--- 4.D/RestaurantLibrary/Chains/Bakery.ts
+++ 4.D/RestaurantLibrary/Chains/Bakery.ts
@@ -1,21 +1,38 @@
-import { Restaurant } from "../../Restaurant";
+import { Restaurant } from "../Devices/dependencies/Restaurant";
 import { Oven } from "../Devices/Oven";
+import { Stove } from "../Devices/Stove";
 
 export class Bakery extends Restaurant {
     private _oven:Oven;
+    private _stove:Stove;
     
     constructor(){
         super();
         this._oven = new Oven();
+        this._stove = new Stove();
     }
 
-prepareFoodOven(item: string): void {
+prepareFood(item: string): void {
+    if (item === "cookies"){
+        this.useOven(item);
+    }else if(item === "crepes"){
+        this.useStove(item);
+    }
+}
+
+private useStove(item: string): void {
+    this._stove.toggleOnDevice();
+    this._stove.cook(item);
+    this._stove.toggleOffDevice();
+}
+
+private useOven(item: string):void {
     this._oven.toggleOnDevice();
     this._oven.cook(item);
     this._oven.toggleOffDevice();
 }
 
-    public get oven():Oven {
-        return this._oven;
-    }
+public get oven():Oven {
+    return this._oven;
+}
 }
